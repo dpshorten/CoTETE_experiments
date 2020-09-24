6319f9a80bb338dac8b50c84ecc00e9a06f35b15
David Shorten <dpshorten@gmail.com>
2020-09-24 19:38:45 +1000

    minor changes

--- figure_2_plotting.py
+++ figure_2_plotting.py
@@ -11,11 +11,14 @@ from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'],
              'size' : 18})
 
-plt.rc('axes', titlesize=18)
-plt.rc('axes', labelsize=18)
-plt.rc('xtick', labelsize=14)
-plt.rc('ytick', labelsize=14)
-plt.rc('figure', titlesize=18)
+plt.rc('axes', titlesize=26)
+plt.rc('axes', labelsize=26)
+plt.rc('xtick', labelsize=22)
+plt.rc('ytick', labelsize=22)
+plt.rc('figure', titlesize=26)
+plt.rc('axes', linewidth=3)
+plt.rc('xtick.major', width=3)
+plt.rc('xtick.minor', width=3)
 
 rc('text.latex', preamble=r'\usepackage{cmbright}')
 
@@ -40,22 +43,23 @@ for key in data_file.keys():
         means = np.array(means)
         stds = np.array(stds)
 
-        sns.lineplot(x = num_events, y = means, palette = "Set3", linewidth = 2, ax = axs[i])
+        sns.lineplot(x = num_events, y = means, palette = "Set3", linewidth = 4, ax = axs[i])
         axs[i].fill_between(num_events, means - stds, means + stds, alpha = 0.5)
         axs[i].hlines(0.0, 0, num_events[-1])
 
         axs[i].set_xscale("log")
         axs[i].set_ylim([-0.5, 0.5])
         axs[i].set_title("$N_U/N_X$ = " + str(mu[i]))
+        #plt.setp(axs[i].spines.values(), linewidth=3)
 
     axs[1].set_ylabel("TE (nats/second)")
     axs[0].set_xlabel("")
     axs[1].set_xlabel("")
     axs[2].set_xlabel("")
     axs[3].set_xlabel("Number of Target Events")
-    plt.subplots_adjust(hspace = 0.3)
+    plt.subplots_adjust(hspace = 0.5)
 
-    plt.show()
+    #plt.show()
     plt.savefig("bias_at_samples_"
-                + str(data_file[key]["k"].value),
-                bbox_inches='tight')
+                + str(data_file[key]["k"].value) +".pdf",
+                bbox_inches='tight', format = 'pdf')
